04e20ce16066a3b5fdab6585bbdd3c5e046e7869
Martin Döring <martin@datenbrei.de>
2015-08-06 18:42:42 +0200

    Fixed sitelist.xml generation

--- admin/include.php
+++ admin/include.php
@@ -24,7 +24,7 @@
 		$xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 		$xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">' . "\n";
 		$files = get_article_list();
-		foreach ($files as $name => $mtime) {
+		foreach ($files as $name) {
 			$article = get_article($name);
 			$xml .= "<url>\n";
 			$xml .= '<loc>' . $config['baseurl'] . $name . "/index.html</loc>\n";
