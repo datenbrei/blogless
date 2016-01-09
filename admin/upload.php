cfe9ae4868aeb43c70f6b5a44ff49cdce556625c
Martin Döring <martin@datenbrei.de>
2016-01-09 11:15:17 +0100

    Fixed: Central authentication also for upload

--- admin/upload.php
+++ admin/upload.php
@@ -8,14 +8,8 @@
 */
 
 	require_once('config.php');
+	require('auth.php');
 
-	// Check Login
-	session_start();
-	if (empty($_COOKIE['blogless']) or empty($_SESSION['login']) or $_COOKIE['blogless'] != $_SESSION['login']) {
-		header('Location: login.php');
-		die("Access denied");
-	}
-	
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$name = (isset($_POST['name']) && $_POST['name'] != '') ? strtolower($_POST['name']) : '';
 		if ($name == '')
